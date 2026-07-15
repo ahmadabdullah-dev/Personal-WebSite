291e9444c84408b5367724292a490e79d633320c
Ahmet Abdullah <ahmadabdullah.formal@gmail.com>
2026-07-16 02:17:10 +0300

    Add theme.ts

--- WEB/src/main.tsx
+++ WEB/src/main.tsx
@@ -1,9 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './app/App.tsx'
-
+import App from './app/App'
+import { ThemeProvider, CssBaseline } from "@mui/material";
+import theme from './lib/theme';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 )
