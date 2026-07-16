a1db03764254297c8760005ccceaaad35e87eb6d
Ahmet Abdullah <ahmadabdullah.formal@gmail.com>
2026-07-17 01:46:54 +0300

    Add logout and currentUser hook

--- WEB/src/lib/hooks/useAuth.ts
+++ WEB/src/lib/hooks/useAuth.ts
@@ -1,10 +1,10 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import agent from "../api/agent";
 import type { LoginDTO, RequestLoginDTO } from "../types/auth";
 import { useNavigate } from "react-router";
 
 export const useAuth = () => {
-
+  const queryClient = useQueryClient();
   const navigate = useNavigate();
 
   const requestLoginAsync = useMutation({
@@ -14,19 +14,28 @@ export const useAuth = () => {
     }, 
   });
 
-
   const loginAsync = useMutation({
     mutationFn: async (creds: LoginDTO) => {
       const response = await agent.post("/auth/login", creds);
       return response.data;
     },
     onSuccess: async () => {
+      await queryClient.invalidateQueries({ queryKey: ["currentUser"] });
       navigate("/admin");
     },
   });
-
+const logoutAsync = useMutation({
+  mutationFn: async () => {
+    await agent.post("/auth/logout");
+  },
+  onSuccess: async () => {
+   await queryClient.removeQueries({ queryKey: ["currentUser"] });
+    navigate("/");
+  },
+});
   return {
     requestLoginAsync,
-    loginAsync
+    loginAsync,
+    logoutAsync
   };
 };
