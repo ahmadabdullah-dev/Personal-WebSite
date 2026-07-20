bb3412649ae575c85e475ffa4c1ad7640a033f4c
Ahmet Abdullah <ahmadabdullah.formal@gmail.com>
2026-07-20 23:35:00 +0300

    Add HeroImageUrl and CVURL fields for Home entity and run migration

---
+++ API/Data/Migrations/20260720203233_Mig_1.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Mig_1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CVURL",
+                table: "Home",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "HeroImageURL",
+                table: "Home",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CVURL",
+                table: "Home");
+
+            migrationBuilder.DropColumn(
+                name: "HeroImageURL",
+                table: "Home");
+        }
+    }
+}
--- API/Entities/Home.cs
+++ API/Entities/Home.cs
@@ -2,10 +2,12 @@
 
 public class Home : BaseEntity
 {
+    public string HeroImageURL { get; set; } = string.Empty;
     public string FullName { get; set; } = string.Empty;
     public string Title { get; set; } = string.Empty;
     public string Bio { get; set; } = string.Empty;
     public string Email {  get; set; } = string.Empty;
     public string GithubLink {  get; set; } = string.Empty;
     public string LinkedInLink {  get; set; } = string.Empty;
+    public string CVURL {  get; set; } = string.Empty;
 }
